f6f11d11444dd9fc1f257a7167ad55ea04501680
Alexander Kartavenko <askemailbox@gmail.com>
2025-08-08 16:38:27 +0300

    gpt-4.1

--- app/bot/openai/openai.go
+++ app/bot/openai/openai.go
@@ -319,7 +319,7 @@ func (o *OpenAI) chatGPTRequestInternal(messages []openai.ChatCompletionMessage)
 	resp, err := o.client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model:     "gpt-5-nano", //openai.GPT4o,
+			Model:     "gpt-4.1", //openai.GPT4o,
 			MaxTokens: o.params.MaxTokensResponse,
 			Messages:  messages,
 		},
